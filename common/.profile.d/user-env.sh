f0d257ddc05b40783b8f720b7d73ed73ede99634
J. Alex Long <jalexlong@proton.me>
2026-06-20 17:22:16 -0500

    Fix bug in common/.profile.d/user-env.sh

--- common/.profile.d/user-env.sh
+++ common/.profile.d/user-env.sh
@@ -19,12 +19,12 @@ elif command -v vi &> /dev/null; then
 fi
 
 # set PATH so it includes user's private bin if it exists
-if [[ -d $HOME/bin ]]; then
+if [ -d $HOME/bin ]; then
     export PATH="$HOME/bin:$PATH"
 fi
 
 # set PATH so it includes user's private bin if it exists
-if [[ -d $HOME/.local/bin ]]; then
+if [ -d $HOME/.local/bin ]; then
     export PATH="$HOME/.local/bin:$PATH"
 fi
 
